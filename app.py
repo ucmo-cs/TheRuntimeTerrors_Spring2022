83d98a47c0b5cef2dccceec091ac369d7c6e2bd3
Justin Kelleher <10236186+JustinKelleher@users.noreply.github.com>
2022-03-18 08:33:27 -0500

    Code clean up

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='dev'
     )
 
     if test_config is None:
@@ -25,44 +24,65 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    mydb = mysql.connector.connect(
+    database = mysql.connector.connect(
         host="localhost",
         user="root",
         password="root",
         database="riskAssessmentTool"
     )
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    @app.route('/riskassessments', methods=['POST'])
-    def process_json():
+    # Create New Risk Assessment Entry in Database Based Off JSON Object
+    @app.route('/riskassessments/create', methods=['POST'])
+    def add_risk_assessment_by_json():
         content_type = request.headers.get('Content-Type')
         if (content_type == 'application/json'):
-            json2 = request.json
-            # print(json)
-            mycursor = mydb.cursor()
-            sql = "INSERT INTO RiskAssessments (TripNumber, LastUpdated, FlightInformation) VALUES (%s, %s, %s)"
-            val = ("1006", datetime.datetime.now(), json.dumps(json2))
-            mycursor.execute(sql, val)
-            mydb.commit()
-            print(mycursor.rowcount, "record inserted.")
-            return json2
+            data = request.json
+            dbcursor = database.cursor()
+            sql = "INSERT INTO RiskAssessments (TripNumber, LastUpdated, FlightInformation, ActiveSubmission) VALUES (%s, %s, %s, %s)"
+            values = (data['FlightInformation']['Release/Trip #'], datetime.datetime.now(), json.dumps(data['FlightInformation']), json.dumps(data))
+            dbcursor.execute(sql, values)
+            database.commit()
+            return data
         else:
             return 'Content-Type not supported!'
 
+    # Create New Blank Risk Assessment Entry in Database Based Off Passed In ID
+    @app.route('/riskassessments/create/<id>', methods=['POST'])
+    def add_blank_risk_assessment_by_id(id):
+        dbcursor = database.cursor()
+        sql = "INSERT INTO RiskAssessments (TripNumber, LastUpdated) VALUES (%s, %s)"
+        values = (id, datetime.datetime.now())
+        dbcursor.execute(sql, values)
+        database.commit()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+    # Update Risk Assessment Entry in Database Based Off JSON Object
+    @app.route('/riskassessments/update', methods=['POST'])
+    def add_risk_assessment_by_json():
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            data = request.json
+            dbcursor = database.cursor()
+            sql = "INSERT INTO RiskAssessments (TripNumber, LastUpdated, FlightInformation, ActiveSubmission) VALUES (%s, %s, %s, %s)"
+            values = (data['FlightInformation']['Release/Trip #'], datetime.datetime.now(), json.dumps(data['FlightInformation']), json.dumps(data))
+            dbcursor.execute(sql, values)
+            database.commit()
+            return data
+        else:
+            return 'Content-Type not supported!'
+
+    # Send BlankFlight.json File
     @app.route('/riskassessments', methods=['GET'])
     def get_blank_risk_assessment():
         return make_response(send_file("BlankFlight.json"))
 
+    # Delete Risk Assessment Entry in Database Based Off JSON Object
     @app.route('/riskassessments/delete/<id>', methods=['DELETE'])
     def delete_risk_assessment_by_id(id):
-        mycursor = mydb.cursor()
+        dbcursor = database.cursor()
         sql = "DELETE FROM RiskAssessments WHERE TripNumber=" + id
-        mycursor.execute(sql)
-        mydb.commit()
+        dbcursor.execute(sql)
+        database.commit()
         return make_response("", 204)
 
     return app
